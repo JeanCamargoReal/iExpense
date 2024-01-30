261ac53a5b696f43fd0a6c5b691ed1fb94532ced
Jean Camargo <jeancamargoreal@icloud.com>
2024-01-30 15:39:16 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jean Camargo on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

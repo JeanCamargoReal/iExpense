d88abdd6e9e8c70f561e864da7fa940e4ed78c20
Jean Camargo <jeancamargoreal@icloud.com>
2024-02-05 14:40:46 -0300

    Building a list we can delete from

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Jean Camargo on 02/02/24.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
---
+++ iExpense/Expenses.swift
@@ -0,0 +1,13 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Jean Camargo on 02/02/24.
+//
+
+import Foundation
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
